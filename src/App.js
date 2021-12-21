9fce7dc9a3f3194c27b867584353318a084aa5a6
alexjesus27 <duranalexis.27@gmail.com>
2021-12-21 15:46:13 -0300

    creando navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Futura pagina de venta de Helados tailandeses
         </p>
         <a
           className="App-link"
